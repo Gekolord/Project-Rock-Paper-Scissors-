b343dd67d4256bee88da537b295ae369dee3fe6f
Gekolord <80892021+Gekolord@users.noreply.github.com>
2023-07-11 16:28:44 +0300

    add functions to javascript that randomly selects computer choice, and compares them

---
+++ javascript.js
@@ -0,0 +1,56 @@
+let playerWins = 0
+let computerWins = 0
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 100);
+    if (number > 0 && number < 33) {
+        return "rock";
+    } else if (number > 32 && number < 66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    switch (true) {
+        case (playerSelection.toLowerCase() == "rock" && computerSelection == "rock"):
+            ++playerWins    
+            ++computerWins
+            return "Tie!";
+    
+        case (playerSelection.toLowerCase() == "rock" && computerSelection == "paper"):
+            ++computerWins
+            return "You lose! Paper beats Rock";
+    
+        case (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors"):
+            ++playerWins
+            return "You win! Rock beats Scissors";
+    
+        case (playerSelection.toLowerCase() == "paper" && computerSelection == "rock"):
+            ++playerWins
+            return "You win! Paper beats Rock";
+
+        case (playerSelection.toLowerCase() == "paper" && computerSelection == "paper"):
+            ++playerWins    
+            ++computerWins
+            return "Tie!";
+
+        case (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors"):  
+            ++computerWins
+            return "You lose! Scissors beat Paper";
+
+        case (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock"):  
+            ++computerWins
+            return "You lose! Rock beats Scissors";
+
+        case (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper"):
+            ++playerWins    
+            return "You Win! Scissors beat Paper";    
+        
+        case (playerSelection.toLowerCase() == "scissors" && computerSelection == "scissors"):
+            ++playerWins    
+            ++computerWins
+            return "Tie!";    
+    }
+}
+
